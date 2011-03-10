50ed1df2779ef394b24c31e79886c1b213857081
Andrew Wang <awang@eecs.berkeley.edu>
2011-03-10 14:01:27 -0800

    copy paste the start of a multithreaded java version

---
+++ iowt-java-mt/Provider.java
@@ -0,0 +1,119 @@
+import java.io.*;
+import java.net.*;
+public class Provider{
+	ServerSocket providerSocket;
+	Socket connection = null;
+	DataOutputStream out;
+	DataInputStream in;
+
+	byte[] buffer = new byte[100];
+	byte[] filebytes;
+
+	Provider()
+	{
+		try {
+			providerSocket = new ServerSocket(8002, 10);
+		} catch(IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	void close()
+	{
+		try {
+			providerSocket.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	void run()
+	{
+		try{
+			System.out.println("Waiting for connection");
+			connection = providerSocket.accept();
+			System.out.println("Connection received from " + connection.getInetAddress().getHostName());
+			out = new DataOutputStream(connection.getOutputStream());
+			in = new DataInputStream(connection.getInputStream());
+			//out.flush();
+
+			// Read in the message from the client
+			int available = in.available();
+			if(available > 100) {
+				available = 100;
+			}
+			int bytes_read = in.read(buffer, 0, available);
+			System.out.println("client>" + buffer);
+
+			// Write a reply
+			sendMessage(filebytes);
+		}
+		catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+		finally{
+			//4: Closing connection
+			try{
+				out.flush();
+				Thread.sleep(1);
+				in.close();
+				out.close();
+				connection.close();
+			}
+			catch(IOException ioException){
+				ioException.printStackTrace();
+			}
+			catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	void sendMessage(byte[] msg)
+	{
+		try{
+			out.write(msg, 0, msg.length);
+			out.flush();
+			System.out.println("Sent " + msg.length + " bytes");
+		}
+		catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+	}
+	void getBytesFromFile(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
+
+        // Get the size of the file
+        long length = file.length();
+        // Create the byte array to hold the data
+        filebytes = new byte[(int)length];
+        // Read in the bytes
+        int offset = 0;
+        int numRead = 0;
+        while (offset < filebytes.length
+               	&& (numRead=is.read(filebytes, offset, filebytes.length-offset)) >= 0)
+        {
+            offset += numRead;
+        }
+        // Ensure all the bytes have been read in
+        if (offset < filebytes.length) {
+            throw new IOException("Could not completely read file "+file.getName());
+        }
+
+        // Close the input stream and return bytes
+        is.close();
+    }
+	public static void main(String args[])
+	{
+		File f = new File(args[0]);
+		while(true) {
+			Provider server = new Provider();
+			try {
+				server.getBytesFromFile(f);
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			server.run();
+			server.close();
+		}
+	}
+}
---
+++ iowt-java-mt/Requester.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.net.*;
+public class Requester{
+	Socket requestSocket;
+	DataOutputStream out;
+	DataInputStream in;
+
+	static final int SIZE = 67108864;
+	//static final int SIZE = 268435456;
+	byte[] buffer = new byte[SIZE];
+	Requester()
+	{
+	}
+
+	void run(String host)
+	{
+		try{
+			requestSocket = new Socket(host, 8002);
+			System.out.println("Connected to localhost in port 2004");
+			out = new DataOutputStream(requestSocket.getOutputStream());
+			in = new DataInputStream(requestSocket.getInputStream());
+			out.flush();
+			//3: Communicating with the server
+			String request = "Hello server!";
+			sendMessage(request.getBytes());
+
+			int available = in.available();
+			if(available > SIZE) {
+				available = SIZE;
+			}
+			int bytes_read = 0;
+			long start_time = System.nanoTime();
+			while(bytes_read < SIZE) {
+				try {
+					bytes_read += in.read(buffer, bytes_read, SIZE-bytes_read);
+				} catch(EOFException eof) {
+					System.out.println("EOF");
+					System.exit(-1);
+				} catch(SocketException se) {
+					System.out.println("ERROR: short read " + bytes_read + 
+							" of expected " + SIZE + " bytes!");
+					se.printStackTrace();
+					break;
+				}
+			}
+			long end_time = System.nanoTime();
+			double request_mbs = bytes_read / Math.pow(2,20);
+			double diff_secs = (double)(end_time - start_time) / (double)1000000000;
+
+			double rate = request_mbs / diff_secs;
+
+			//System.out.println("server " + bytes_read  + ">" + buffer);
+			System.out.println("Rate: " + rate);
+		}
+		catch(UnknownHostException unknownHost){
+			System.err.println("You are trying to connect to an unknown host!");
+		}
+		catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+		finally{
+			//4: Closing connection
+			try{
+				in.close();
+				out.close();
+				requestSocket.close();
+			}
+			catch(IOException ioException){
+				ioException.printStackTrace();
+			}
+		}
+	}
+	void sendMessage(byte[] msg)
+	{
+		try{
+			out.write(msg, 0, msg.length);
+			out.flush();
+		}
+		catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+	}
+	public static void main(String args[])
+	{
+		Requester client = new Requester();
+		client.run(args[0]);
+	}
+}
+
